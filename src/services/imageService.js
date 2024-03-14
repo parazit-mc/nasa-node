d15584a921e7388a76984e00b57743a2e1a14281
parazit-mc <beloff185.7@gmail.com>
2024-03-14 14:00:51 +0100

    proxy 12 (add schemas && validations)

--- src/services/imageService.js
+++ src/services/imageService.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const qs = require('qs');
 
 async function getImage(req){
-    console.log("Authenticating user " + req.body.userId + " " + req.body.userName + ", api key " + req.body.apiKey);
+    console.log(`Authenticating user ${req.body.userId} ${req.body.userName}, api key ${req.body.apiKey}`);
     const uri = buildImageUri(req.body.apiKey);
     const response = await axios.get(uri);
     return response.data.latest_photos[0].img_src;
